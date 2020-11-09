88884552fb4f790cfee131edcea83bd584a070d3
Adjani Valerius <2068687@talnet.nl>
2020-11-09 13:38:55 +0100

    PythonBasic_2_taak1

---
+++ 2-vervolg datatypes/taak01-lists/PythonBasic_lists.py
@@ -0,0 +1,5 @@
+maanden= ['januari', 'februari', 'maart', 'april', 'mei', 'juni','july','augustus','september','oktober','november','december']
+print (maanden)
+print (maanden[6])
+maanden[6] = "juli"
+print (maanden[6])
