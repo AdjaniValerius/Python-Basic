710fe18f616de2bbf8c4a10f577a8a43a8d794eb
Adjani Valerius <2068687@talnet.nl>
2021-02-08 12:01:00 +0100

    taak 1 klaar

---
+++ 3-control-flow/taak01 -if en else/if.py
@@ -0,0 +1,8 @@
+a = 2#integer
+
+if(a==5): #FALSE
+    print("a is gelijk aan 5")
+else if(a== 100):
+    print("a is 100 ")
+
+print("En nu zijn we er klaar mee")
---
+++ 4-functions/taak01 functies/PythonBasic_functions1.py
@@ -0,0 +1,5 @@
+def rekenGradenOm(gradenCelcius):
+	return gradenCelcius * 9/5 + 32
+
+resultaat = rekenGradenOm(25)
+print(resultaat)
